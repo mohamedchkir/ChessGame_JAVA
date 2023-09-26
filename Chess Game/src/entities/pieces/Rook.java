36c43dc5b6f8f35f4884106c2ec57f16012bb780
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-09-27 00:15:41 +0100

    travailler rook deplacement

--- Chess Game/src/entities/pieces/Rook.java
+++ Chess Game/src/entities/pieces/Rook.java
@@ -4,6 +4,7 @@ import entities.board.Piece;
 import entities.board.Square;
 import enums.PieceSide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -18,7 +19,54 @@ public class Rook extends Piece {
     }
 
     @Override
+
+
+
     public List<Square> abilityMoves(Square[][] board) {
-        return null;
+        List<Square> squareList = new ArrayList<>();
+        Square actualSquare = this.getSquare();
+        int x = actualSquare.getX();
+        int y = actualSquare.getY();
+
+        // Mouvements vers le haut
+        for (int i = y - 1; i >= 0; i--) {
+            Square square = board[i][x];
+            if (square.getPiece() == null) {
+                squareList.add(square);
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = y + 1; i < 8; i++) {
+            Square square = board[i][x];
+            if (square.getPiece() == null) {
+                squareList.add(square);
+            } else {
+                break;
+            }
+        }
+
+        for (int i = x - 1; i >= 0; i--) {
+            Square square = board[y][i];
+            if (square.getPiece() == null) {
+                squareList.add(square);
+            } else {
+                break;
+            }
+        }
+
+        for (int i = x + 1; i < 8; i++) {
+            Square square = board[y][i];
+            if (square.getPiece() == null) {
+                squareList.add(square);
+            } else {
+                break;
+            }
+        }
+
+        return squareList;
     }
+
+
 }
